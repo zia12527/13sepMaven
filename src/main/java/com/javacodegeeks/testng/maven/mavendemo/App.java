0646082b74d26ada2746b3bb51c7bb6a60133956
root <ziarehman534@gmail.com>
2018-09-13 17:34:45 +0530

    uploading maven project to eclipse

---
+++ src/main/java/com/javacodegeeks/testng/maven/mavendemo/App.java
@@ -0,0 +1,19 @@
+package com.javacodegeeks.testng.maven.mavendemo;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class App 
+{
+	public static WebDriver driver ;
+	
+    public static void main( String[] args )
+    {
+    	  @Test
+    	   public void exampleOfTestNgMaven() 
+    	  {
+    	 System.out.println("This is TestNG-Maven Example");
+    	  }
+
+    }
+}
